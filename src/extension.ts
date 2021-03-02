cb535b96cfbffb78d351ed30b93250af495460d8
Chris Daly <irondoctorchris@gmail.com>
2021-03-01 20:48:07 -0800

    Removed comments and debug code

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import getCharacterQuickPickItems from "./CharacterSet";
 import { QuickPickItemExtended } from "./QuickPickItemExtended";
@@ -7,15 +5,8 @@ import {
     addCharacterToRecentlyUsed,
     getRecentlyUsed,
 } from "./RecentlyUsedCharactersStorage";
-import UnicodeCharacter from "./UnicodeCharacter";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.debug('Extension "unicodepalette" activating');
-
     context.subscriptions.push(
         vscode.commands.registerTextEditorCommand(
             "unicodepalette.insertCharacter",
@@ -68,7 +59,4 @@ export function activate(context: vscode.ExtensionContext) {
     );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-    console.debug('Extension "unicodepalette" deactivating');
-}
+export function deactivate() {}
