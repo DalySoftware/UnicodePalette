f18b9790201df5dabf615783e7a90c5ce98e0e04
Chris Daly <irondoctorchris@gmail.com>
2021-03-01 17:20:04 -0800

    Removed old debug code

--- src/CharacterSet.ts
+++ src/CharacterSet.ts
@@ -3,16 +3,8 @@ import { QuickPickItemExtended } from "./QuickPickItemExtended";
 import { loadParsedData } from "./UnicodeDataLoad";
 
 const getCharacterQuickPickItems = async () => {
-    let items: QuickPickItemExtended[] = [];
-
     const unicodeCharacters = await loadParsedData();
-    const unicodeQuickPickItems = unicodeCharacters.map(char =>
-        char.asQuickPickItem(),
-    );
-
-    items = [...items, ...unicodeQuickPickItems];
-
-    return items;
+    return unicodeCharacters.map(char => char.asQuickPickItem());
 };
 
 export default getCharacterQuickPickItems;
