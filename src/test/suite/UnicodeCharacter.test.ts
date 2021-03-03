11098191cc81c2ead646d6116dcd7b5cae4b013a
Chris Daly <irondoctorchris@gmail.com>
2021-03-02 19:27:38 -0800

    Added unit test for UnicodeCharacter class

---
+++ src/test/suite/UnicodeCharacter.test.ts
@@ -0,0 +1,39 @@
+import { describe } from "mocha";
+import * as assert from "assert";
+import UnicodeCharacter from "../../UnicodeCharacter";
+
+describe("UnicodeCharacter", function () {
+    describe("#toString()", function () {
+        it("should return the unicode character itself", function () {
+            const character = new UnicodeCharacter(
+                "0060",
+                "GRAVE ACCENT",
+                "SYMBOL, MODIFIER",
+                ["grave accent", "backtick"],
+            );
+
+            assert.strictEqual(character.toString(), "`");
+        });
+    });
+
+    describe("#asQuickPickItem()", function () {
+        it("should format correctly as a QuickPickItemExtended", function () {
+            const character = new UnicodeCharacter(
+                "0060",
+                "GRAVE ACCENT",
+                "Sk",
+                ["grave accent", "backtick"],
+            );
+
+            const quickPickItem = character.asQuickPickItem();
+
+            assert.strictEqual(quickPickItem.character, character);
+            assert.strictEqual(quickPickItem.description, "hex:0060\tint:96");
+            assert.strictEqual(
+                quickPickItem.detail,
+                "[Symbol, Modifier]\tgrave accent, backtick",
+            );
+            assert.strictEqual(quickPickItem.label, "`\tGRAVE ACCENT");
+        });
+    });
+});
